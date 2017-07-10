18456cbde028d98bac7f984cb5ce66be822e8b8b
miont <miontq@gmail.com>
2017-07-10 18:53:37 +0300

    Добавлена обработка аргументов командной строки

--- python/astra_tables_compose.py
+++ python/astra_tables_compose.py
@@ -2,8 +2,9 @@
 import fnmatch
 import os
 import re
+import argparse
 
-def compose_astra_HTML_tables(input_dir, target_path, files_list=[]):
+def compose_astra_HTML_tables(input_dir, target_path, files_list=[], multithread=False):
     """
     Объединение набора HTML-таблиц сгенерированных с помощью FastReport
     Требования: классы во всех документах должны обозначаться s0,s1,s2,...,
@@ -13,7 +14,7 @@ def compose_astra_HTML_tables(input_dir, target_path, files_list=[]):
         input_dir: Директория, откуда брать таблицы
         target_path: Имя файла с полным путем с объединенной таблицей 
         files_list: Список файлов для объединения, если пустой, то используются все HTML-файлы из input_dir         
-        
+        multithread (boolean): Флаг многопоточности
     Returns:
         none
     """
@@ -21,9 +22,6 @@ def compose_astra_HTML_tables(input_dir, target_path, files_list=[]):
     # TODO предусмотреть что стили могут начинаться не с s
     # TODO предусмотреть что стили могут нумероваться не подряд
     # TODO реализовать многопоточную обработку HTML-документов    
-
-    # Считывание аргументов командной строки
-    # TODO реализовать с использованием argparse    
     
     # Если передан пустой список, берем все html-файлы из директории
     if files_list == None or len(files_list) == 0:
@@ -51,7 +49,7 @@ def compose_astra_HTML_tables(input_dir, target_path, files_list=[]):
         # Считывание стилей
         m = re.search(r'<style\s+type\s*=\s*"text/css">(.+)</style>', content, flags=re.DOTALL)
         if m == None:
-            raise Exception('Error during processing re.search(r\'<style\s+type\s*=\s*"text/css">(.+)</style>\'...')
+            raise TablesComposerException('Ошибка при обработке регулярного выражения \'<style\s+type\s*=\s*"text/css">(.+)</style>\'')
         styles_content = m.group(1)
         
         # Удаляем символы комментария        
@@ -73,7 +71,7 @@ def compose_astra_HTML_tables(input_dir, target_path, files_list=[]):
         # Считывание таблиц
         for m in re.finditer(r'(<table[^<]+>.+</table>)', content, flags=re.DOTALL):
             if m == None:
-                raise Exception('Error during processing re.search(r\'<table[^<]+>.+</table>\'')
+                raise TablesComposerException('Ошибка при обработке регулярного выражения \'<table[^<]+>.+</table>\'')
             table_content = m.group(1)
             
             # Корректировка номеров стилей
@@ -90,7 +88,8 @@ def compose_astra_HTML_tables(input_dir, target_path, files_list=[]):
         max_class_num_prev += classes_count_cur
         
     print('Завершение обработки')
-    print('Обработано %d документов, %d таблиц' % (idx+1, table_num))
+    print('Обработано: документов %d, таблиц %d' % (idx+1, table_num))
+    
     print('Генерация выходного файла')
     output_lines = []
     output_lines.append('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">\n')
@@ -119,8 +118,79 @@ def compose_astra_HTML_tables(input_dir, target_path, files_list=[]):
     print('Выходной файл создан') 
     return
 
+def parse_args():
+    """
+    Считывание аргументов командной строки
+    
+    Args:    
+        none
+    
+    Returns:
+        dict:
+        {
+        'dir': директория с HTML-файлами
+        'output': путь к выходному файлу
+        'files': список HTML-файлов
+        'multithread': флаг многопоточной обработки
+        }
+    """
+    parser = argparse.ArgumentParser(description='Склеивание сводных таблиц в HTML')
+    parser.add_argument('-d', '--dir', type=str, help='директория с HTML-файлами')
+    parser.add_argument('-f', '--file', type=str, nargs='+', help='список HTML-файлов')
+    parser.add_argument('-o', '--output', type=str, help='путь к выходному файлу')
+    parser.add_argument('-m', '--multithread', type=int, help='флаг многопоточной обработки')
+    args_parser_result = parser.parse_args()
+
+    # Словарь с результатами обработки
+    args = {}
+    if(args_parser_result.dir and args_parser_result.output):
+        args['dir'] = args_parser_result.dir
+        args['output'] = args_parser_result.output
+    else:
+        raise ArgsParserException('Не переданы необходимые аргументы: -d, -o')
+    
+    # Парсинг списка файлов
+    if (args_parser_result.file):
+        files_string = ' '.join(args_parser_result.file)
+        if(files_string.find("\"") == -1):
+            args['files'] = files_string.split(' ')
+        else:
+            files_list = []
+            for m in re.finditer(r'"([\w\s]+\.\w+)"', files_string):
+                if(m == None):
+                    raise ArgsParserException('Ошибка при обработке re.finditer(r'("[\w\s]+")+', files_string)')
+                file_name = m.group(1)
+                files_list.append(file_name)
+            args['files'] = files_list
+    else:
+        args['files'] = []
+    
+    if(args_parser_result.multithread):
+        if(args_parser_result.multithread == 1):
+             args['multithread'] = True
+        else:
+             args['multithread'] = False
+    else:
+        args['multithread'] = False
+        
+    return args
+    
+def run_from_command_line():
+    """
+    Запуск из командной строки        
+    """
+    args = parse_args()
+    compose_astra_HTML_tables(args['dir'], args['output'], args['files'])
+    
+class TablesComposerException(Exception):
+    pass
+class ArgsParserException(TablesComposerException):
+    pass
+
 if __name__ == '__main__':
     
+    run_from_command_line()
+    
 #    compose_astra_HTML_tables('../test','../test/CombinedTable.html',['макс напряж в отводах.HTML', 'макс напряж в тройниках.HTML', 'макс напряж в прямых.HTML', 'нагрузки на патрубки арматуры.HTML', 'нагрузки на оборуд и конструкции.HTML'])
-    compose_astra_HTML_tables('../test','../test/CombinedTable.html')
+#    compose_astra_HTML_tables('../test','../test/CombinedTable.html')
     
